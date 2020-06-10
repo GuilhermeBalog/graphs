1eeae82bbe1f3749d0e0d276a306e34d28252862
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2020-06-09 23:28:20 -0300

    Adiciona o algoritmo de Kruskal

---
+++ graphs/KruskalMST.java
@@ -0,0 +1,36 @@
+package graphs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class KruskalMST implements IMST {
+    private Queue<Edge> mst;
+    private double weight;
+
+    public KruskalMST(EdgeWeightedGraph G){
+        mst = new LinkedList<>();
+        MinPQ<Edge> pq = new MinPQ<Edge>(G.E());
+        UF uf = new UF(G.V());
+
+        while(!pq.isEmpty() && mst.size() < G.V() - 1){
+            Edge e = pq.delMin();
+            int v = e.either();
+            int w = e.other(v);
+
+            if(uf.find(v) == uf.find(w))
+                continue;
+
+            uf.union(v, w);
+            mst.add(e);
+            weight += e.weight();
+        }
+    }
+
+    public Iterable<Edge> edges(){
+        return mst;
+    }
+
+    public double weight(){
+        return weight;
+    }
+}
